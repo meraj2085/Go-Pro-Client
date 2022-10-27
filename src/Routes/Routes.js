0e4d478c3b8b21f61b78930accef567342f1e2c8
meraj2085 <samirmeraj60@gmail.com>
2022-10-27 10:00:25 +0600

    fixing navbar

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
 import Main from "../layouts/Main";
@@ -8,6 +9,10 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Main></Main>,
     children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
       {
         path: "/login",
         element: <Login></Login>,
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,70 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>GoPro</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Link to="#features">Courses</Link>
-              <Link to="#pricing">FAQ</Link>
-              <Link to='/login'>Blog</Link>
-            </Nav>
-            <Nav>
-              <Link to="/login">Login</Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <header className="p-4 bg-gray-900 text-white">
+        <div className="container flex justify-between mx-auto px-5 py-1">
+          <div>
+            <p
+              rel="noopener noreferrer"
+              aria-label="Back to homepage"
+              className="flex items-center text-2xl"
+            >
+              Go Pro
+            </p>
+          </div>
+          <div className="items-center flex-shrink-0 hidden lg:flex">
+            <Link to="/">
+              <button className="self-center rounded px-5 text-xl hover:text-purple-600">
+                Home
+              </button>
+            </Link>
+            <Link to="/contact">
+              <button className="self-center rounded px-5 text-xl hover:text-purple-600">
+                Courses
+              </button>
+            </Link>
+            <Link to="/about">
+              <button className="self-center rounded px-5 text-xl hover:text-purple-600">
+                FAQ
+              </button>
+            </Link>
+            <Link to="/pricing">
+              <button className="self-center rounded px-5 text-xl hover:text-purple-600">
+                Blog
+              </button>
+            </Link>
+          </div>
+          <div className="hidden xl:flex items-center space-x-5">
+            <Link to="/user">
+              <img className="h-8 w-8 rounded-[50%]" src="/" alt="" />
+            </Link>
+            <Link to="/pricing">
+              <button className="self-center rounded px-5 text-xl hover:text-purple-600">
+                Blog
+              </button>
+            </Link>
+          </div>
+          <button className="p-4 lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6 text-gray-800"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </header>
     </div>
   );
 };
