1eb92107121e637b47363a4f306f181126dc3b3c
meraj2085 <samirmeraj60@gmail.com>
2022-10-27 09:07:23 +0600

    adding leftside nav

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
+import LeftSideNav from "../components/LeftSideNav/LeftSideNav";
 
 const Main = () => {
   return (
     <>
       <Header></Header>
-      {/* <Container>
+      <Container>
         <Row>
           <Col lg="3" className="d-none d-lg-block">
-            <h1>left side</h1>
+            <LeftSideNav></LeftSideNav>
           </Col>
           <Col lg="9">
             <Outlet></Outlet>
           </Col>
         </Row>
-      </Container> */}
-      <Outlet></Outlet>
+      </Container>
     </>
   );
 };
