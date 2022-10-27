4f5fb04b8598e7e2ef2c452326cfe37930561d7a
meraj2085 <samirmeraj60@gmail.com>
2022-10-27 18:43:43 +0600

    adding pdf

--- src/components/CourseDetails/CourseDetails.jsx
+++ src/components/CourseDetails/CourseDetails.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { FaCloudDownloadAlt } from "react-icons/fa";
+import Pdf from "react-to-pdf";
 
 const CourseDetails = () => {
   const course = useLoaderData();
+  const ref = React.createRef();
   const { id, name, picture, details, price } = course;
+
   return (
     <div className="my-16 mx-20">
-      <div className="card lg:card-side bg-base-100 shadow-xl">
+      <div ref={ref} className="card lg:card-side bg-base-100 shadow-xl">
         <figure>
           <img src={picture} alt="Album" />
         </figure>
         <div className="card-body">
           <div className="flex justify-between">
             <h2 className="card-title">{name}</h2>
+
+            <Pdf targetRef={ref} filename="code-example.pdf">
+              {({ toPdf }) => <button onClick={toPdf}>Generador de Pdf</button>}
+            </Pdf>
+
             <button
               type="button"
               className="px-4 text-2xl py-2 font-semibold border rounded-full border-gray-800 text-gray-800"
