9f4c6c9558cc72b8543c2ffb99d4e01634fcebdb
meraj2085 <samirmeraj60@gmail.com>
2022-10-27 21:44:20 +0600

    adding secret env keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdC_vR1S6dswk7bJXOXR5w_UJCRP8gT1M",
-  authDomain: "go-pro-9a816.firebaseapp.com",
-  projectId: "go-pro-9a816",
-  storageBucket: "go-pro-9a816.appspot.com",
-  messagingSenderId: "718751259222",
-  appId: "1:718751259222:web:44c5062bae8549082379f7"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
