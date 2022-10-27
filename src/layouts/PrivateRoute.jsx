e86762d9afba96447e65456519db6c6d1b548ffa
meraj2085 <samirmeraj60@gmail.com>
2022-10-27 20:32:33 +0600

    adding spinner

--- src/layouts/PrivateRoute.jsx
+++ src/layouts/PrivateRoute.jsx
@@ -1,24 +1,107 @@
-import React from 'react';
-import { useContext } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { AuthContext } from '../context/UserContext';
+import React from "react";
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../context/UserContext";
 
-const PrivateRoute = ({children}) => {
-     const {user, loading} = useContext(AuthContext)
-     const location = useLocation();
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
-     if (loading) {
-          return (
-            <div className="text-center">
-                <h1>Loading...</h1>
-            </div>
-          );
-        }
-      
-        if (!user) {
-          return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
-        }
-        return children;
+  if (loading) {
+    return (
+      <div className="flex justify-center h-screen">
+        <div
+          aria-label="Loading..."
+          role="status"
+          class="flex items-center space-x-2"
+        >
+          <svg
+            class="h-12 w-12 animate-spin stroke-gray-500"
+            viewBox="0 0 256 256"
+          >
+            <line
+              x1="128"
+              y1="32"
+              x2="128"
+              y2="64"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="60.1"
+              x2="173.3"
+              y2="82.7"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="224"
+              y1="128"
+              x2="192"
+              y2="128"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="195.9"
+              y1="195.9"
+              x2="173.3"
+              y2="173.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="128"
+              y1="224"
+              x2="128"
+              y2="192"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="195.9"
+              x2="82.7"
+              y2="173.3"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="32"
+              y1="128"
+              x2="64"
+              y2="128"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+            <line
+              x1="60.1"
+              y1="60.1"
+              x2="82.7"
+              y2="82.7"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="24"
+            ></line>
+          </svg>
+          <span class="text-xs font-medium text-gray-500">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+  return children;
 };
 
 export default PrivateRoute;
